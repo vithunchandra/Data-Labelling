96cfa670cf223b49fcdffbffaed4b85f5e033e9a
Valr0c <valent.rocky1302@gmail.com>
2023-11-08 21:17:35 +0700

    add some component for requester

--- Frontend/src/route.tsx
+++ Frontend/src/route.tsx
@@ -1,7 +1,9 @@
 import RequesterDashboard from "./pages/requester/RequesterDashboard";
+import CreateTask from "./pages/requester/CreateTask";
 import Marketplace from "./pages/worker/Marketplace";
 import WorkerDashboard from "./pages/worker/WorkerDashboard";
 import WorkerTask from "./pages/worker/WorkerTask";
+import TopUp from "./pages/requester/TopUp";
 
 export const workerNavigation = [
   {
@@ -30,5 +32,17 @@ export const requesterNavigation = [
     icon: <i className="bi bi-house"></i>,
     element: <RequesterDashboard />,
     path: "/requester",
+  },
+  {
+    name: "Create Task",
+    icon: <i className="bi bi-createtask"></i>,
+    element: <CreateTask />,
+    path: "/requester/create+task",
+  },
+  {
+    name: "Top Up",
+    icon: <i className="bi bi-topup"></i>,
+    element: <TopUp />,
+    path: "/requester/top+up",
   },
 ];
