1aab6ee4ce7e4556f0a4fe25c3aba2b42a77e9a6
MarioHoliwono <90135814+MarioHoliwono@users.noreply.github.com>
2023-11-13 14:39:39 +0700

    minor change in admin

---
+++ Frontend/src/interface/UserInterface.tsx
@@ -0,0 +1,7 @@
+export default interface User{
+    _id: string,
+    name: string,
+    role: string,
+    credibility: number,
+    wallet: number
+}
--- Frontend/src/pages/admin/AdminDashboard.tsx
+++ Frontend/src/pages/admin/AdminDashboard.tsx
@@ -1,31 +1,12 @@
 import user from "../../dummy_data/user.json";
+import UserList from '../../components/admin/UserList';
 
 export default function AdminDashboard() {
   return (
-    <div className="container">
-      <h1 className="my-3">Admin Liat User</h1>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Role</th>
-            <th>Credibility</th>
-            <th>Wallet</th>
-          </tr>
-        </thead>
-        <tbody>
-          {user.map((user, index) => (
-            <tr key={index}>
-              <td>{user._id}</td>
-              <td>{user.name}</td>
-              <td>{user.role}</td>
-              {user.credibility !== 0 ? <td>{user.credibility}</td> : <td></td>}
-              {user.wallet !== 0 ? <td>Rp. {user.wallet}</td> : <td></td>}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="container-fluid p-3 mt-4 bg-white rounded-2 shadow-sm">
+        <div className="fw-bold fs-4 mb-2">User</div>
+      <UserList user={user}></UserList>
+
     </div>
   );
 }
