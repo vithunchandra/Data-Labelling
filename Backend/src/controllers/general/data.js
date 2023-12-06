aabeab2f06c22c0f0f9de861bdda1e015f640562
dragoox <datawolfxxx5@gmail.com>
2023-12-06 14:40:01 +0700

    bulk create data

--- Backend/src/controllers/general/data.js
+++ Backend/src/controllers/general/data.js
@@ -1,6 +1,59 @@
 const { Data, Task, Task_Type, User } = require("../../models");
 const { queryHuggingFaceSummary } = require("../../utils/helper_function");
 
+const create_data_helper = async (task_now, text) => {
+  const task_type_now = await Task_Type.findById(task_now.task_type);
+  const char_price = task_type_now.price;
+
+  if (String(task_type_now.name).toLowerCase() == "ai summary checking") {
+    const summary_now = await queryHuggingFaceSummary(text);
+    text = text + "\n\nSummary:" + summary_now[0]["generated_text"];
+  }
+
+  const price = char_price * text.length;
+
+  const new_data = await Data.create({
+    text: text,
+    price: price,
+    task: task_now._id,
+    labels: [],
+  });
+
+  const updated_task = await Task.findByIdAndUpdate(task_now._id, {
+    $push: { data: new_data._id },
+  });
+
+  return new_data;
+};
+
+const update_data_helper = async (data_now, text, possible_label = []) => {
+  const price_char_now = data_now.price / data_now.text.length;
+  const new_price = price_char_now * text.length;
+  //////////////////////////////////////////
+
+  const task_now = await Task.findById(data_now.task);
+  const task_type_now = await Task_Type.findById(task_now.task_type);
+
+  if (String(task_type_now.name).toLowerCase() == "ai summary checking") {
+    const summary_now = await queryHuggingFaceSummary(text);
+    text = text + "\n\nSummary:" + summary_now[0]["generated_text"];
+  }
+
+  if (String(task_type_now.name).toLowerCase() == "classification") {
+  }
+
+  const updated_data = await Data.findByIdAndUpdate(
+    data_now._id,
+    {
+      text: text,
+      price: new_price,
+    },
+    { new: true }
+  );
+
+  return updated_data;
+};
+
 const create_data = async (req, res) => {
   let { task_id, text } = req.body;
   const requester_id = req.user._id;
@@ -30,28 +83,55 @@ const create_data = async (req, res) => {
     });
   }
 
-  const task_type_now = await Task_Type.findById(task_now.task_type);
-  const char_price = task_type_now.price;
-
-  if (String(task_type_now.name).toLowerCase() == "ai summary checking") {
-    const summary_now = await queryHuggingFaceSummary(text);
-    text = text + "\n\nSummary:" + summary_now[0]["generated_text"];
-  }
-
-  const price = char_price * text.length;
-
-  const new_data = await Data.create({
-    text: text,
-    price: price,
-    task: task_id,
-    labels: [],
-  });
-
-  const updated_task = await Task.findByIdAndUpdate(task_id, {
-    $push: { data: new_data._id },
-  });
+  const new_data = await create_data_helper(task_now, text);
 
   return res.json(new_data);
+};
+
+const create_bulk_data = async (req, res) => {
+  let { task_id, data } = req.body;
+
+  const requester_id = req.user._id;
+  if (String(req.user.role) != "requester") {
+    return res.status(403).json({
+      message: "Data must be created by requester",
+    });
+  }
+
+  if (!requester_id || !task_id || !data) {
+    return res.status(400).json({
+      message: "Please provide all required fields",
+    });
+  }
+
+  if (data.length == 0) {
+    return res.status(400).json({
+      message: "Data must not be empty",
+    });
+  }
+
+  const task_now = await Task.findById(task_id);
+  if (!task_now) {
+    return res.status(404).json({
+      message: "Task Not Found",
+    });
+  }
+
+  // console.log(task_now.requester, requester_id);
+  if (String(task_now.requester) !== String(requester_id)) {
+    return res.status(403).json({
+      message: "Data must be created by the same requester who create the task",
+    });
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    // use helper to create data
+    const new_data = await create_data_helper(task_now, data[i]);
+  }
+
+  return res.status(200).json({
+    msg: "All data created sucesfully!",
+  });
 };
 
 const edit_data = async (req, res) => {
@@ -69,16 +149,8 @@ const edit_data = async (req, res) => {
       message: "Data Not Found",
     });
   }
-
-  ////////////////// CAREFUL!!! ////////////
-  const price_char_now = data_now.price / data_now.text.length;
-  const new_price = price_char_now * text.length;
-  //////////////////////////////////////////
-
-  const updated_data = await Data.findByIdAndUpdate(data_id, {
-    text: text,
-    price: new_price,
-  });
+  // use helper to update data
+  const updated_data = await update_data_helper(data_now, text);
 
   return res.status(200).json({
     msg: "Suceed!",
@@ -181,4 +253,5 @@ module.exports = {
   create_data,
   edit_data,
   label_data,
+  create_bulk_data,
 };
